037074e6e5181a9e469c87e8700da9676a0e6b82
sergej.romanov <sergej.romanov@advcake.com>
2022-04-11 01:23:01 +0500

    final

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -46,8 +46,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-
-console.log(323232)
